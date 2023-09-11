115221ba979d841e7691d54316bee1c831f2cdea
Darya <darya.kolobova@inbox.ru>
2023-09-11 14:17:35 +0300

    add new files

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("intl.accept_languages", user_language)
+    browser=None
+    #browser = webdriver.Chrome(options=options)
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(firefox_profile=fp)   
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_main_page.py
@@ -0,0 +1,13 @@
+import pytest
+import time
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
