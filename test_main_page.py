bb6cfe88e9ae0f9231f2b25ba2d065dfbb57f797
Darya <darya.kolobova@inbox.ru>
2023-09-12 08:38:43 +0300

    changed test with Page Object rules

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.main_page import MainPage
 import pytest
 import time
 import math
@@ -8,6 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page=MainPage(browser,link)
+    page.open()
+    page.go_to_login_page()
