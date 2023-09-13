62edddc18495a1bc7c44c4dac6fb33b78a4e5101
Darya <darya.kolobova@inbox.ru>
2023-09-13 14:00:52 +0300

    loginpage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click() 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        # *, он указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать. 
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import pytest
 import time
 import math
@@ -10,5 +11,10 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page=MainPage(browser,link)
+    login_page=LoginPage(browser,browser.current_url)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_link()
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
